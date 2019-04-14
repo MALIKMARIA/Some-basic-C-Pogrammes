4e8f711de7c1f93f2e0a346e81826224bf20b159
Malik <31213374+MALIKMARIA@users.noreply.github.com>
2019-04-14 20:12:33 +0500

    Add files via upload

---
+++ Array_of_Structures/main.c
@@ -0,0 +1,45 @@
+/*   E X A M P L E   11.26
+     PASSING ARRAY OF STRUCTURES TO A FUNCTION */
+
+
+#include <stdio.h>
+
+typedef struct{
+    char *name;
+    int accno;
+    char acctype;
+    float acctbalance;
+
+}record;
+
+void search(record table[],int acctno);
+int main()
+{
+    record customer[3]= {
+            {"smith",45,'A',50.0},
+            {"jones",46,'B',55.0},
+            {"Charlie",47,'C',60.0}
+                         };
+    record *pt;
+    printf("Enter account no. to display complete information of customer \n Else enter 0");
+    int account_no_to_be_searched;
+    scanf("%d",&account_no_to_be_searched );
+    while(account_no_to_be_searched != 0)
+    {
+        search(customer , account_no_to_be_searched );
+
+    }
+
+
+
+    return 0;
+}
+void search(record table[],int account_no_to_be_searched )
+{
+    for(int i=0; i<3; i++)
+    {
+        if(table[i].accno == account_no_to_be_searched )
+            printf(table[i].name);
+    }
+
+}
