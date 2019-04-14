78083e7f2635bdf8031b51d2dd33dae06b714e21
Malik <31213374+MALIKMARIA@users.noreply.github.com>
2019-04-14 21:47:10 +0500

    Add files via upload

---
+++ strlen/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <mem.h>
+
+int main() {
+
+    int l;
+    l= strlen("Maria Khalid");
+    printf("%d \n",l);
+
+    char arr[]="Maria";
+    int m = strlen(arr);
+    printf("%d",m);
+
+
+    return 0;
+}
